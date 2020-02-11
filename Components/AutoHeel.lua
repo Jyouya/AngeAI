499a30d32c9fe23fd68b4ab96848d801251a2bd0
Will <willwest8xp@gmail.com>
2020-02-11 16:51:27 -0500

    Fixed memory leak in AutoHeel

--- Components/AutoHeel.lua
+++ Components/AutoHeel.lua
@@ -1,10 +1,19 @@
 local lastActive = GetTick()
 local lastOwnerX, lastOwnerY = 0, 0
+local autoHeel = false
 
 function AutoHeel(event, next)
 
   if lastOwnerX == World.ownerPosition.x and lastOwnerY == World.ownerPosition.y then
-    if World.tick > lastActive + 60000 then return Events:emit('heel') end
+    if World.tick > lastActive + 60000 and not autoHeel then
+      autoHeel = true
+      Events:once('release', function(event, next)
+        lastActive = GetTick()
+        autoHeel = false
+        next()
+      end)
+      return Events:emit('heel')
+    end
   else
     lastOwnerX, lastOwnerY = World.ownerPosition.x, World.ownerPosition.y
     lastActive = World.tick
