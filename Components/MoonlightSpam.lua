fa93080b127f9d92876ea11ab6433e6f8595f57a
Will <willwest8xp@gmail.com>
2022-05-26 01:32:23 -0400

    Makes moonlight level selectable in mob config

--- Components/MoonlightSpam.lua
+++ Components/MoonlightSpam.lua
@@ -1,5 +1,6 @@
 local prevAttackTarget
 local boltCounter
+local moonlightLevel
 
 function MoonlightSpam(event, next)
   if event.usedSkill then return next() end
@@ -8,12 +9,13 @@ function MoonlightSpam(event, next)
     -- local mobId = GetV(V_HOMUNTYPE, AttackTarget)
     prevAttackTarget = AttackTarget
     boltCounter = event.target.mobConfig.bolts or 0
+    moonlightLevel = event.target.mobConfig.moonlightLevel or 5
   end
   if boltCounter ~= 0 and (SkillDelay < World.tick or boltCounter < 0) and
-    World.mySP > 20 then
+    World.mySP > 4 * moonlightLevel then
     if boltCounter > 0 then boltCounter = boltCounter - 1 end
 
-    SkillObject(World.myId, 5, 8009, AttackTarget)
+    SkillObject(World.myId, moonlightLevel, 8009, AttackTarget)
     SetSkillDelay(400)
     event.usedSkill = true
   end
